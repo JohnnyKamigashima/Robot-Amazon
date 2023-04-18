a954ede1c9593c7df25a5678073531722e1fa492
Johnny Kamigashima <johnny@jhk.app>
2023-04-18 00:11:32 -0300

    All

---
+++ Locators/locators.py
@@ -0,0 +1,8 @@
+# Page Locators
+
+url = "https://www.amazon.com.br/"
+botaoProcura = "id=nav-search-submit-button"
+campoBusca = "id=twotabsearchtextbox"
+adicionaAoCarrinho = "id=add-to-cart-button"
+botaoCarrinho = "id=nav-cart"
+subtotalTexto = "id=sc-subtotal-label-buybox"
